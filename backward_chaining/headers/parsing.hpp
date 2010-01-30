a213257fb7f817e0cb6e17e3f259c58be24f7a24
sarazi_t <sarazi_t@epitech.net>
2010-01-30 02:45:58 +0000

    parsing OK :D

--- backward_chaining/headers/parsing.hpp
+++ backward_chaining/headers/parsing.hpp
@@ -13,18 +13,6 @@
 #include "Rule.h"
 #include "Fact.h"
 
-class truc
-{
-    std::string a;
-
-    public:
-        truc(std::string w)
-        {
-            this->a = w;
-        }
-
-};
-
 namespace client
 {
     namespace qi = boost::spirit::qi;
@@ -32,9 +20,10 @@ namespace client
     namespace phoenix = boost::phoenix;
 
     template <typename Iterator>
-        bool parse_numbers(Iterator first, Iterator last, std::vector<Rule>& rules)
+        bool parse_numbers(Iterator first, Iterator last, std::vector<Rule *>& rules, std::vector<Fact *>& factes)
         {
             using qi::print;
+            using qi::lexeme;
             using qi::phrase_parse;
             using qi::char_;
             using qi::_1;
@@ -44,25 +33,93 @@ namespace client
             using phoenix::push_back;
             using phoenix::ref;
 
+            qi::rule<Iterator, std::string(), ascii::space_type> pars_rule;
+            qi::rule<Iterator, std::string(), ascii::space_type> name_rule;
             qi::rule<Iterator, std::string(), ascii::space_type> condition;
             qi::rule<Iterator, std::string(), ascii::space_type> result;
 
+            qi::rule<Iterator, std::string(), ascii::space_type> pars_fact;
+            qi::rule<Iterator, std::string(), ascii::space_type> value;
+
+            qi::rule<Iterator, std::string(), ascii::space_type> pars_search;
+            
+            std::string str_name_rule = "";
+            std::string str_condition = "";
+            std::string str_result = "";
+
+            std::vector<char> tab_variable;
+            std::string str_value = "";
+
+            std::vector<char> tab_search;
+            
+            // Pars Rule
+            name_rule %= char_('R') >> +(char_ - ':');
             result %= +(char_);
             condition %= +(char_ - '=');
+            pars_rule %= (name_rule[ref(str_name_rule) += _1]
+                        >> lit(':')
+                        >> condition[ref(str_condition) += _1]
+                        >> lit('=')
+                        >> result[ref(str_result) += _1]);
+
+            // Pars Fact
+            value %= +(char_ - '=');
+            pars_fact %= lit('F') >> lit(':')
+                         >> +((char_ - '=')[push_back(ref(tab_variable), _1)])
+                         >> lit('=')
+                         >> value[ref(str_value) += _1];
+
+            // Pars Search
+            pars_search %= lit('S') >> lit(':') 
+                         >> +(char_[push_back(ref(tab_search), _1)]);
 
             bool r = phrase_parse(first, last,
 
                     (
-                     lit('R') >> lit(':')
-                     >> condition 
-                     >> lit('=')
-                     >> result
-                     //)[push_back(ref(rules), _1, _2)]
+                     pars_rule
+                     | pars_fact
+                     | pars_search
                     )
                     ,
                     space);
 
-            
+            if (str_name_rule != "" && str_condition != "" && str_result != "")
+            {
+                size_t size_name_rule = str_name_rule.size();
+                str_condition.erase(0, str_name_rule.size());
+                str_result.erase(0, str_name_rule.size() + str_condition.size());
+                std::cout << "name : " << str_name_rule << std::endl;
+                std::cout << "condition : " << str_condition << std::endl;
+                std::cout << "result : " << str_result << std::endl;
+                rules.push_back(new Rule(str_name_rule, str_condition, str_result));
+            }
+            std::cout << "size : " << tab_variable.size() << " value : " << str_value << std::endl;
+            if (tab_variable.size() > 0 && str_value != "")
+            {
+                str_value.erase(0, tab_variable.size());
+                bool bool_value;
+                if (str_value == "true")
+                    bool_value = 1;
+                else
+                    bool_value = 0;
+
+                std::vector<char>::iterator it;
+                for ( it = tab_variable.begin() ; it < tab_variable.end(); ++it )
+                {
+                    std::cout << "variable : " << *it << std::endl;
+                    std::cout << "value : " << bool_value << std::endl;
+                    factes.push_back(new Fact(*it, Tribool(bool_value), 1));
+                }
+            }
+            if (tab_search.size() > 0)
+            {
+                std::vector<char>::iterator it;
+                for ( it = tab_search.begin() ; it < tab_search.end(); ++it )
+                {
+                    std::cout << "search : " << *it << std::endl;
+                    factes.push_back(new Fact(*it, Tribool(2), 0));
+                }
+            }
 
             if (first != last) // fail if we did not get a full match
                 return false;
--- backward_chaining/src/main.cpp
+++ backward_chaining/src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 
-//#include "parsing.hpp"
+#include "parsing.hpp"
 #include "RuleSet.h"
 #include "FactSet.h"
 #include "Tribool.h"
@@ -12,165 +12,48 @@
 std::vector<Rule *> construct_rules();
 std::vector<Fact *> construct_facts();
 
-void    parse_file(char const * file_name)
+void    parse_file(char const * file_name, std::vector<Rule *>& v, std::vector<Fact *>& f)
 {
-  //     std::filebuf fb;
-  //     std::istream is(&(fb));
+    std::filebuf fb;
+    std::istream is(&(fb));
 
-  //     fb.open(file_name, std::ios::in);
+    fb.open(file_name, std::ios::in);
 
-  //     std::string str;
-  //     while (getline(is, str))
-  //     {
-  //         truc ee(15);
-  //         std::vector<Rule> v;
-  //         if (client::parse_numbers(str.begin(), str.end(), v))
-  //         {
-  //             std::cout << "-------------------------\n";
-  //             std::cout << "Parsing succeeded\n";
-  //             std::cout << str << " Parses OK: " << std::endl;
-
-  //             //for (std::vector<Rule>::size_type i = 0; i < v.size(); ++i)
-  //             //    std::cout << i << ": " << v[i] << std::endl;
-
-  //             std::cout << "\n-------------------------\n";
-  //         }
-  //         else
-  //         {
-  //             std::cout << "-------------------------\n";
-  //             std::cout << "Parsing failed\n";
-  //             //std::cout << str << std::endl;
-  //             std::cout << "-------------------------\n";
-  //         }
-  //     }
-
-  //     fb.close();
-}
-
-int	main(int ac, char **av)
-{
-  if (av[1] == NULL || ac != 2)
+    std::string str;
+    while (getline(is, str))
     {
-      std::cerr << "usage: ./BackwardChaining File.txt"<< std::endl;
-      return (1);
+        std::cout << "-------------------------\n";
+        if (client::parse_numbers(str.begin(), str.end(), v, f))
+        {
+            std::cout << "Parsing success\n";
+        }
+        else
+        {
+            std::cout << "Parsing failed\n";
+        }
+        std::cout << "-------------------------\n";
     }
-  parse_file(av[1]);
 
-  std::vector<Fact *> facts;
-  std::vector<Rule *> rules;
-  facts = construct_facts();
-  rules = construct_rules();
-  RuleSet	ruleset(rules);
-  FactSet	factset(facts);
-  Fact* searchFact = new Fact('F', Tribool(2), 0);
-  Tribool result = resolve_fact(searchFact, ruleset, factset);
-  searchFact->display();
-  //std::cout<< "Result :" << result << std::endl;
-  return 0;
+    fb.close();
 }
 
-std::vector<Rule *> construct_rules()
+int main(int ac, char **av)
 {
-  std::vector<Rule *> rules;
-  //   Test1
-  //   Rule* r1 = new Rule("R1", "B", "A");
-  //   Rule* r2 = new Rule("R2", "C", "A");
-  //   Rule* r3 = new Rule("R3", "D & E", "B");
-  //   Rule* r4 = new Rule("R4", "G & H", "F");
-  //   Rule* r5 = new Rule("R5", "I & J", "G");
-  //   Rule* r6 = new Rule("R6", "G", "H");
-  //   Rule* r7 = new Rule("R7", "L & M", "K");
-  //   Rule* r8 = new Rule("R8", "O & P", "L & N");
-  //   Rule* r9 = new Rule("R9", "N", "M");
+    if (av[1] == NULL || ac != 2)
+    {
+        std::cerr << "usage: ./BackwardChaining File.txt"<< std::endl;
+        return (1);
+    }
+    std::vector<Fact *> facts;
+    std::vector<Rule *> rules;
 
-  //   rules.push_back(r1);
-  //   rules.push_back(r2);
-  //   rules.push_back(r3);
-  //   rules.push_back(r4);
-  //   rules.push_back(r5);
-  //   rules.push_back(r6);
-  //   rules.push_back(r7);
-  //   rules.push_back(r8);
-  //   rules.push_back(r9);
+    parse_file(av[1], rules, facts);
 
-  //test2
-  // Rule* r1 = new Rule("R1", "B & C", "A");
-  //   Rule* r2 = new Rule("R2", "D & E", "B");
-  //   Rule* r3 = new Rule("R3", "B", "C");
-  //   rules.push_back(r1);
-  //   rules.push_back(r2);
-  //   rules.push_back(r3);
-  //TEST3
-  // Rule* r1 = new Rule("R1", "B & C", "A");
-  //   Rule* r2 = new Rule("R2", "D & !E", "B");
-  //   Rule* r3 = new Rule("R3", "B", "C");
+    RuleSet   ruleset(rules);
+    FactSet   factset(facts);
 
-  //   rules.push_back(r1);
-  //   rules.push_back(r2);
-  //   rules.push_back(r3);
-  
-  //TEST4
-  Rule* r1 = new Rule("R1", "B | C ", "A");
-  Rule* r2 = new Rule("R2", "D & !E", "B");
-  Rule* r3 = new Rule("R3", "B", "C");
-  Rule* r4 = new Rule("R4", "!G & H", "F");
-  Rule* r5 = new Rule("R5", "!E", "!G");
-  Rule* r6 = new Rule("R6", "D", "H");
-  rules.push_back(r1);
-  rules.push_back(r2);
-  rules.push_back(r3);
-  rules.push_back(r4);
-  rules.push_back(r5);
-  rules.push_back(r6);
-  return rules;
-};
-
-std::vector<Fact *> construct_facts()
-{
-  std::vector<Fact *> facts;
-
-  //test1
-  //   Fact* d = new Fact('D', Tribool(1), 1);
-  //   Fact* e = new Fact('E', Tribool(1), 1);
-  //   Fact*	i = new Fact('I', Tribool(1), 1);
-  //   Fact*	j = new Fact('J', Tribool(1), 1);
-  //   Fact* o = new Fact('O', Tribool(1), 1);
-  //   Fact* p = new Fact('P', Tribool(1), 1);
-
-  //   facts.push_back(d);
-  //   facts.push_back(e);
-  //   facts.push_back(i);
-  //   facts.push_back(j);
-  //   facts.push_back(o);
-  //   facts.push_back(p);
-
-  //test2
-  //   Fact* d = new Fact('D', Tribool(1), 1);
-  //   Fact* e = new Fact('E', Tribool(1), 1);
-
-
-  //   facts.push_back(d);
-  //   facts.push_back(e);
-
-  //test3
-  //   i) DE = true
-  // 		   ii) D = true E = false
-  // Fact* d = new Fact('D', Tribool(1), 1);
-  //   Fact* e = new Fact('E', Tribool(0), 1);
-
-  //   facts.push_back(d);
-  //   facts.push_back(e);
-
-  //test4
-//   i) D = false E = true
-// 		   ii) DE = true
-// 			iii) D = true E = false
-  Fact* d = new Fact('D', Tribool(1), 1);
-  Fact* e = new Fact('E', Tribool(0), 1);
-
-  facts.push_back(d);
-  facts.push_back(e);
-
-  return facts;
-};
-
+    Fact* searchFact = new Fact('E', Tribool(2), 0);
+    Tribool result = resolve_fact(searchFact, ruleset, factset);
+    std::cout<< "Result :" << result << std::endl;
+    return 0;
+}
