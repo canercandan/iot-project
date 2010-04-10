49a508b7ff049eccc0b928312c4ebc44eaa89cd2
schiav_r <schiav_r@epitech.net>
2010-04-10 16:34:42 +0000

    loli

--- builder/builder.py
+++ builder/builder.py
@@ -2,10 +2,17 @@ import PyQt4
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import *
 
+class Box(QRect):
+    def __init__(self, t_left=QPoint(), b_right=QPoint()):
+        QRect.__init__(self, t_left, b_right)
+
+        self.focus = 0
+
 class BuilderWidget(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
+        self.focused = 0
         self.shiftPressed = 0
         self.leftButtonPressed = 0
         self.beginPos = QPoint()
@@ -14,12 +21,27 @@ class BuilderWidget(QtGui.QWidget):
         self.setGeometry(300, 300, 600, 600)
         self.setWindowTitle('IOT Builder')
 
-    def selectRegion(self, pos):
-        for region in self.list:
-            if region.contains(pos):
-                print "lolz"
+    def selectBox(self, pos):
+        topBox = 0
+        for box in self.list:
+            if box.normalized().contains(pos):
+                topBox = box
+            box.focus = 0
+        self.focused = topBox
+        if topBox:
+            topBox.focus = 1
 
     def keyPressEvent(self, keyEvent):
+        if self.focused:
+            if keyEvent.key() == QtCore.Qt.Key_Left:
+                self.focused.translate(-1, 0)
+            elif keyEvent.key() == QtCore.Qt.Key_Right:
+                self.focused.translate(1, 0)
+            elif keyEvent.key() == QtCore.Qt.Key_Up:
+                self.focused.translate(0, -1)
+            elif keyEvent.key() == QtCore.Qt.Key_Down:
+                self.focused.translate(0, 1)
+            self.repaint()
         if keyEvent.key() == QtCore.Qt.Key_Shift:
             self.shiftPressed = 1
         elif keyEvent.key() == QtCore.Qt.Key_Escape:
@@ -36,6 +58,8 @@ class BuilderWidget(QtGui.QWidget):
 
     def mouseMoveEvent(self, mouseEvent):
         if self.leftButtonPressed:
+            # if self.focused:
+            #     self.focused.
             self.endPos = QPoint(mouseEvent.pos())
             self.repaint()
         else:
@@ -46,7 +70,8 @@ class BuilderWidget(QtGui.QWidget):
             self.leftButtonPressed = 1
             self.beginPos = QPoint(mouseEvent.pos())
             if self.shiftPressed == 0:
-                self.selectRegion(mouseEvent.pos())
+                self.selectBox(mouseEvent.pos())
+            self.repaint()
         else:
             QtGui.QWidget.mousePressEvent(self, mouseEvent)
 
@@ -55,7 +80,9 @@ class BuilderWidget(QtGui.QWidget):
             self.leftButtonPressed = 0
             if self.shiftPressed == 1:
                 self.endPos = mouseEvent.pos()
-                self.list.append(QRect(self.beginPos, mouseEvent.pos()))
+                va1 = QPoint(min(self.beginPos.x(), mouseEvent.pos().x()), min(self.beginPos.y(), mouseEvent.pos().y()))
+                va2 = QPoint(max(self.beginPos.x(), mouseEvent.pos().x()), max(self.beginPos.y(), mouseEvent.pos().y()))
+                self.list.append(Box(va1, va2))
                 self.repaint()
         else:
             QtGui.QWidget.mouseReleaseEvent(self, mouseEvent)
@@ -64,15 +91,23 @@ class BuilderWidget(QtGui.QWidget):
         paint = QtGui.QPainter()
         paint.begin(self)
 
-        color = QtGui.QColor(0, 0, 0)
-        color.setNamedColor('#d4d4d4')
+        color = QtGui.QColor('#d4d4d4')
+        colorFocus = QtGui.QColor('#aaaaaa')
         paint.setPen(color)
 
         paint.setBrush(QtGui.QColor(0, 0, 255, 80))              # RVB, opacity
 
         if self.leftButtonPressed and self.shiftPressed:
             paint.drawRect(self.beginPos.x(), self.beginPos.y(), self.endPos.x() - self.beginPos.x(), self.endPos.y() - self.beginPos.y())
+
         if self.list:
             for x in self.list:
-                paint.drawRect(x)
+                if x.focus == 1:
+                    print "focus == 1"
+                    paint.setBrush(QtGui.QColor(0, 255, 0, 80))              # RVB, opacity
+                    paint.drawRect(x)
+                    paint.setBrush(QtGui.QColor(0, 0, 255, 80))              # RVB, opacity
+                else:
+                    paint.drawRect(x)
         paint.end()
+
--- builder/main.py
+++ builder/main.py
@@ -5,12 +5,12 @@ from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import *
 
 import builder
-from builder import BuilderWidget
+from builder import *
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     builder = BuilderWidget()
     builder.setWindowOpacity(0.4)                                # transparence
     builder.setWindowFlags(QtCore.Qt.FramelessWindowHint)        # barre bleu
-    builder.show()                                     # fullscreen
-    app.exec_()                                             # main loop
+    builder.show()                                               # fullscreen
+    app.exec_()                                                  # main loop
