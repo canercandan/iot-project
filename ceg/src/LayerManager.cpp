6299ec15b6fc49712e7fc40b114b577393289d15
Caner Candan <caner@candan.fr>
2010-04-24 14:58:45 +0000

    + license header added to all source files

--- ceg/src/LayerManager.cpp
+++ ceg/src/LayerManager.cpp
@@ -1,3 +1,23 @@
+// -*- mode: c++; c-indent-level: 4; c++-member-init-indent: 8; comment-column: 35; -*-
+
+/* IOT Copyright (C) 2010 CEG development team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Authors: CEG <ceg@ionlythink.com>, http://www.ionlythink.com
+ */
+
 #include <algorithm>
 
 #include <QtGui/QApplication> // debug
@@ -80,4 +100,14 @@ void LayerManager::createLayers(std::list<Ceg::Window> & windows)
 bool    LayerManager::actionHandler(IAction & anAction)
 {
     return (anAction.exec(*this));
+}
+
+AbstractScene*	LayerManager::getCurrentLayer()
+{
+    return (*this->_currentLayer);
+}
+
+ICommunicationGraphicalServer*	LayerManager::getComGs()
+{
+    return (this->_comGs);
 }
